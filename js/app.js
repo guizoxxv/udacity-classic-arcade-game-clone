1f212e836b16629b1889d2844fdd3a568c9b30e3
guizoxxv <guizoxxv@gmail.com>
2017-01-11 18:00:33 -0200

    Udacity review #1 fix

--- js/app.js
+++ js/app.js
@@ -13,7 +13,13 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
-    randomBug(this);
+    // Set enemy x start position
+    this.x = 0;
+    // Set enemy position to random value on yArray
+    this.y = yArray[Math.floor(Math.random() * yArray.length)];
+    // Set enemy velocity to random value between 10 and 30 and multiply by the level to increase difficulty
+    this.veloc = Math.floor((Math.random() * 30) + 10) * level;
+
 };
 
 // Update the enemy's position, required method for game
@@ -26,11 +32,24 @@ Enemy.prototype.update = function(dt) {
 
     // Refresh enemy when it reaches the canvas extrem right
     if (this.x > 504) {
-        randomBug(this);
+      // Set enemy x start position
+      this.x = 0;
+      // Set enemy position to random value on yArray
+      this.y = yArray[Math.floor(Math.random() * yArray.length)];
+      // Set enemy velocity to random value between 10 and 30 and multiply by the level to increase difficulty
+      this.veloc = Math.floor((Math.random() * 30) + 10) * level;
     }
 
     // Check if enemy hit player
-    hitPlayer(this);
+    // A hit occurs when the enemy position is at the same time between the player x and y position.
+    // It is necessary to add a value to player.x and player.y in order to check for a hit on the boundaries of the player image
+    if ((this.x > player.x - 75 && this.x < player.x + 75) && (this.y > player.y - 75 && this.y < player.y + 75)) {
+        // initial player position
+        player.x = 202;
+        player.y = 373.5;
+        // return to level 1 when player get hit
+        level = 1;
+    }
 
     // Display the level variable value to .level span when update
     document.querySelector('.level span').innerHTML = level;
@@ -49,31 +68,36 @@ var Player = function() {
     this.x = 202;
     this.y = 373.5;
 
+    // player image
     this.sprite = 'images/char-boy.png';
+
 };
 
 Player.prototype.handleInput = function(keyPress) {
     // Change position according to input keyPress. if statments only allows movement inside canvas bounderies.
     switch (keyPress) {
         case 'up':
-            player.y = player.y - 83;
+            this.y = this.y - 83;
             if (player.y < 0) {
-                levelUp();
+              this.x = 202;
+              this.y = 373.5;
+              level++;
+              // console.log(level);
             }
             break;
         case 'down':
-            if (player.y < 373.5) {
-                player.y = player.y + 83;
+            if (this.y < 373.5) {
+                this.y = this.y + 83;
             }
             break;
         case 'left':
-            if (player.x > 0) {
-                player.x = player.x - 101;
+            if (this.x > 0) {
+                this.x = this.x - 101;
             }
             break;
         case 'right':
-            if (player.x < 404) {
-                player.x = player.x + 101;
+            if (this.x < 404) {
+                this.x = this.x + 101;
             }
             break;
     }
@@ -87,33 +111,6 @@ Player.prototype.update = function(dt) {
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-var randomBug = function(enemy) {
-    enemy.x = 0;
-    // Set enemy position to random value on yArray
-    enemy.y = yArray[Math.floor(Math.random() * yArray.length)];
-    // Set enemy velocity to random value between 10 and 30 and multiply by the level to increase difficulty
-    enemy.veloc = Math.floor((Math.random() * 30) + 10) * level;
-};
-
-var hitPlayer = function(enemy) {
-    // A hit occurs when the enemy position is at the same time between the player x and y position.
-    // It is necessary to add a value to player.x and player.y in order to check for a hit on the boundaries of the player image
-    if ((enemy.x > player.x - 75 && enemy.x < player.x + 75) && (enemy.y > player.y - 75 && enemy.y < player.y + 75)) {
-        // initial player position
-        player.x = 202;
-        player.y = 373.5;
-        // return to level 1 when player get hit
-        level = 1;
-    }
-};
-
-var levelUp = function() {
-    player.x = 202;
-    player.y = 373.5;
-    level++;
-    // console.log(level);
 };
 
 // Now instantiate your objects.
